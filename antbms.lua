c71298fbcceefa1870c7cc9ee8af1540e04886c9
zwim <36999612+zwim@users.noreply.github.com>
2025-01-16 15:19:43 +0100

    V4.1.2 Bug: missing clearDataAge in switch:toggle

--- antbms.lua
+++ antbms.lua
@@ -15,28 +15,9 @@ else
     http = require("socket.http")
 end
 
+local ESP32_HARD_RESET_COMMAND = require("antbms-reset")
+
 local READ_DATA_SIZE = 140
--- command to hard reset a connected ESP32 device
-
--- quick and dirty, as the python-esptool interactin in gentoo as of 2024/06/26 seems broken
-local RESET_COMMAND = "esptool.py"
-local RESET_COMMAND_ARGS =
-    " --chip esp32 --port /dev/ttyUSB0 --baud 460800 --before default_reset --after hard_reset run"
-if os.execute(RESET_COMMAND .. " version 2> /dev/null") ~= 0 then
-    RESET_COMMAND = "python /usr/lib/python-exec/python3.12/esptool.py"
-    if os.execute(RESET_COMMAND .. " version 2> /dev/null") ~= 0 then
-        RESET_COMMAND = "python /usr/share/esp-idf/components/esptool_py/esptool/esptool.py"
-        if os.execute(RESET_COMMAND .. " version 2> /dev/null") ~= 0 then
-            RESET_COMMAND = "uhubctl"
-            RESET_COMMAND_ARGS = " -l 1-1 -d 10 -p 2 -a cycle"
-        --                                   ^------------------ delay 10s
-        end
-    end
-end
-
-print("Reset command dedected: " .. RESET_COMMAND)
-
-local ESP32_HARD_RESET_COMMAND = "killall minicom; killall tio; " .. RESET_COMMAND .. RESET_COMMAND_ARGS
 
 -- Get data at `pos` in `buffer` attention lua table starts with 1
 -- whereas the protocol is defined for a C-buffer starting with 0
@@ -329,7 +310,7 @@ function AntBMS:evaluateData(force)
             os.execute(ESP32_HARD_RESET_COMMAND)
             os.execute("date")
             os.execute("echo 'Could not read bsm.data -> reboot ESP32 -> echo sleeping " .. config.sleep_time .. "'")
-            util.sleep_time(config.sleep_time)
+            util.sleep_time(2 * config.sleep_time)
             return false
         end
         self.answer = {}
