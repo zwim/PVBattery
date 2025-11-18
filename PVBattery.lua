967bfeec18c9671814af94fcf626e0f67d0a0851
zwim <36999612+zwim@users.noreply.github.com>
2025-11-18 19:26:00 +0100

    new

--- PVBattery.lua
+++ PVBattery.lua
@@ -2,7 +2,7 @@
 -- ###############################################################
 -- CONFIGURATION
 -- ###############################################################
-local VERSION = "V5.2.1"
+local VERSION = "V5.2.2"
 
 local Profiler = nil
 -- profiler from https://github.com/charlesmallah/lua-profiler
